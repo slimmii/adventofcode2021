00735979b3508e70d70eea0d2964e38d422eafc5
Similon Andie (p118722) <andie.similon@ap.be>
2021-12-06 13:19:32 +0100

    New solution

--- day5/Day5/src/main/kotlin/Main.kt
+++ day5/Day5/src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import kotlin.collections.HashMap
+import kotlin.math.sign
 
 fun main(args: Array<String>) {
     val input = File("input.txt")
@@ -10,5 +12,50 @@ fun main(args: Array<String>) {
     val diagram: Diagram = Diagram(input)
 
     println(diagram.getSafePoints().size)
+
+}
+
+class Coords constructor(x: Int, y: Int) {
+    var x: Int = x
+    var y: Int = y
+
+    override fun toString(): String {
+        return "($x,$y)";
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Coords) {
+            return other.x == x && other.y == y;
+        }
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return x.hashCode() + y.hashCode();
+    }
+}
+
+class Diagram constructor(input : List<List<List<Int>>>) {
+    var diagram : HashMap<Coords, Int> = HashMap()
+    init {
+        for (line in input) {
+            var from : Coords = Coords(line[0][0], line[0][1])
+            val to : Coords = Coords(line[1][0], line[1][1])
+            var xMover = sign((to.x-from.x).toDouble()).toInt()
+            var yMover = sign((to.y-from.y).toDouble()).toInt()
+            while (from != to) {
+                this.diagram[from] = (this.diagram[from] ?: 0) + 1
+                from = Coords(from.x+xMover, from.y+yMover)
+                println(from)
+            }
+            this.diagram[from] = (this.diagram[from] ?: 0) + 1
+        }
+
+        println(this.diagram);
+    }
+
+    fun getSafePoints() : List<Coords> {
+        return this.diagram.filter { e -> e.value >= 2 }.keys.toList()
+    }
 
 }
